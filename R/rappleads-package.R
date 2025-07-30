6277fa069397eb41eadbe309a38c85097c9c108a
selesnow <selesnow@gmail.com>
2025-07-30 13:18:00 +0300

    Изменение зависимостей, добавил dplyr

--- R/rappleads-package.R
+++ R/rappleads-package.R
@@ -4,6 +4,7 @@
 ## usethis namespace: start
 #' @importFrom openssl read_key
 #' @importFrom snakecase to_snake_case
+#' @importFrom dplyr rename_with
 #' @import httr2
 #' @import tidyr
 ## usethis namespace: end
